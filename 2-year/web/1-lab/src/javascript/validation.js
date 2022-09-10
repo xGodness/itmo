7f2313787a1713b2b0f9ab5eb97f84a6ef6eedf0
= <alexei.gotovko@gmail.com>
2022-09-10 16:56:39 +0300

    intermediate commit

---
+++ 2-year/web/1-lab/src/javascript/validation.js
@@ -0,0 +1,11 @@
+function validate() {
+    let raw_y = document.getElementById("input-y").value;
+    let raw_r = document.getElementById("input-r").value;
+
+    let y = parseFloat(raw_y);
+    let r = parseFloat(raw_r);
+
+    if (Number.isNaN(y) || Number.isNaN(r)) {
+
+    }
+}
