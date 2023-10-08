3a2e8917386d41ccf6ddd4828ebec4a3a5d3c3ff
xGodness <alexei.gotovko@gmail.com>
2023-10-08 18:43:15 +0300

    remove redundant files; add .gitignore

--- 3-year/operating-systems/xv6-riscv/user/printf.d
+++
@@ -1,2 +0,0 @@
-user/printf.o: user/printf.c kernel/types.h kernel/stat.h user/user.h \
- /home/xgodness/itmo/3-year/operating-systems/sc-dt/riscv-gcc/lib/gcc/riscv64-unknown-elf/12.2.1/include/stdarg.h
